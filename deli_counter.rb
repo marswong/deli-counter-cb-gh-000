f25686393f55af729ce250531d7f1ed0580f2cad
Mars Wong <marswong618@gmail.com>
2018-01-24 14:44:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 def line(array)
   if array.length == 0
-    "The line is currently empty."
+    return "The line is currently empty."
   else
     array.each_index { |x| array[x] = "#{x + 1}. #{array[x]}" }
+    return "The line is currently: #{array.join(' ')}"
   end
 end
